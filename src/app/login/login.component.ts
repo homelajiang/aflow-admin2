6b84f740268f56a44c4dd1bb53ca3114146ff434
homelajiang <homelajiang@163.com>
2019-11-26 23:55:59 +0800

    修改评论界面 添加登陆界面

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+import {SnackBar} from '../utils/snack-bar';
+import {BlogService} from '../blog/blog.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
 
-  constructor() { }
+  logging = false;
+
+  constructor(private snackBar: MatSnackBar, private blogService: BlogService, private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    if (!this.username) {
+      SnackBar.open(this.snackBar, '用户名能为空');
+      return;
+    }
+
+    if (!this.password) {
+      SnackBar.open(this.snackBar, '用户密码不能为空');
+      return;
+    }
+
+    this.logging = true;
+
+    this.blogService.login(this.username, this.password)
+      .subscribe(auth => {
+        this.logging = false;
+      }, err => {
+        this.logging = false;
+        SnackBar.open(this.snackBar, err);
+      });
   }
 
 }
